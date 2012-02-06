c373859e9f56d346f8d6146f632f1984f5c8936e
Alexandra Hauser <alexandra_ha@freenet.de>
2012-02-06 14:59:25 +0100

    sign in fertig

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 MlclspApp::Application.routes.draw do
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/signup', :to => 'users#new'
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   match '/produkte', :to => 'pages#produkte'
   root :to => 'pages#home'
